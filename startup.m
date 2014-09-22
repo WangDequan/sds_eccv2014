23c7a76922ee45ab79b54ade396eda531d1a7f04
WangDequan <wangdequan@yehao-ESC4000-G2-Series.(none)>
2014-09-22 16:57:01 +0800

    Run demo_sds.m

---
+++ startup.m
@@ -0,0 +1,11 @@
+addpath(pwd);
+addpath('extern/liblinear/matlab');
+addpath(fullfile(pwd, 'evaluation'));
+addpath(fullfile(pwd, 'feature_extractor'));
+addpath(fullfile(pwd, 'misc'));
+addpath(fullfile(pwd, 'preprocessing'));
+addpath(fullfile(pwd, 'refinement'));
+addpath(fullfile(pwd, 'region_classification'));
+addpath(fullfile(pwd, 'extern/caffe/matlab/caffe'));
+addpath(fullfile(pwd, 'evaluation'));
+addpath(fullfile(pwd, 'extern/MCG-PreTrained'));
